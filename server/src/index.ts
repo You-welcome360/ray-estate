8ff99dbb4b659c18028d2c59300e5b2dab2bfef6
Raymond Amoah <razbowy.70@gmail.com>
2026-01-14 11:54:17 +0000

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,14 @@ import cors from "cors";
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { authMiddleware } from './middleware/authMiddleware';
-import tenantRoute from './routes/tenantRoute'
-import managerRoute from './routes/managerRoute'
 
 // ROUTE IMPORT
+import tenantRoute from './routes/tenantRoutes'
+import managerRoute from './routes/managerRoutes'
+import propertyRoute from './routes/propertyRoutes'
+import leaseRoute from './routes/leaseRoutes'
+import applicationRoute from './routes/applicationRoutes'
+
 
 
 //CONFIGURATIONS
@@ -27,8 +31,11 @@ app.get('/',(req,res)=>{
     res.send('This is home route')
 })
 
-app.use("/tenants", authMiddleware(['tenant']),tenantRoute)
-app.use("/managers", authMiddleware(['manager']),managerRoute)
+app.use("/tenants", authMiddleware(['tenant']),tenantRoute);
+app.use("/managers", authMiddleware(['manager']),managerRoute);
+app.use('/properties',propertyRoute);
+app.use('/leases',leaseRoute);
+app.use('/applications',applicationRoute);
 
 
 //SERVER
---
+++ server/src/routes/propertyRoutes.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { authMiddleware } from "../middleware/authMiddleware";
+import {
+  createProperty,
+  getProperties,
+  getProperty,
+  getPropertyLeases,
+} from "../controllers/propertyControllers";
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const router = express.Router();
+
+router.get("/", getProperties);
+router.get("/:id", getProperty);
+router.get("/:id/leases", getPropertyLeases);
+router.post(
+  "/",
+  authMiddleware(["manager"]),
+  upload.array("photos"),
+  createProperty
+);
+export default router;  
