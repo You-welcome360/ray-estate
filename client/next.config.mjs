06d3cd2f7bc23c87f06ba30e9b516abb97198b02
You-welcome360 <116973419+You-welcome360@users.noreply.github.com>
2026-01-15 03:02:32 +0000

    Refactor image remotePatterns in next.config.mjs
    Updated remotePatterns to allow all AWS hosts.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'rental-s3-pictures.s3.eu-west-2.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'example.com',
+            port: '',
+            pathname: '/**'
+        },
+        {
+            protocol: 'https',
+            hostname: '*.amazonaws.com',
+            port: '',
+            pathname: '/**'
+        },
+        {
+            protocol: 'https',
+            hostname: 'rental-s3-pictures.s3.eu-west-2.amazonaws.com',
+            port: '',
+            pathname: '/**'
+        }]
+    }
 };
 
 export default nextConfig;
