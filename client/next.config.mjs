3f9b8577ead55cdba52e915413f34981305a2340
You-welcome360 <116973419+You-welcome360@users.noreply.github.com>
2026-01-15 00:17:34 +0000

    Add ESLint ignoreDuringBuilds option
    Added ESLint configuration to ignore builds.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,19 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: 'https',
-            hostname: 'example.com',
-            port: '',
-            pathname: '/**'
-        },
-        {
-            protocol: 'https',
-            hostname: 'rental-s3-pictures.s3.eu-west-2.amazonaws.com',
-            port: '',
-            pathname: '/**'
-        }]
-    }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'rental-s3-pictures.s3.eu-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
